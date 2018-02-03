fbc4d6fe3fcd4fa2b94c090f9b6a6932be20e449
root <root@localhost.localdomain>
2018-02-03 01:44:05 +0100

    hellomellow

--- BLACK_AFRO/config.py
+++ BLACK_AFRO/config.py
@@ -1,21 +1,33 @@
-from tkinter import Tk, Label, Button
+from tkinter import Tk, Label, Button, Frame
 
 class Welcome:
     def __init__(self, master):
-        self.master = master
-        master.title("Black Jack simulator v1.0")
+        self._master = master
+        self._master.title("A.I. BlackJack Sim v1.0")
+        self._master.geometry('{}x{}'.format(460, 350))
+        
 
-        self.label = Label(master, text="Welcome to advanced Black Jack simulator")
-        self.label.pack()
+#        self._top_frame = Frame(self._master, bg='cyan', width=450, height=50, pady=3)
 
-        self.greet_button = Button(master, text="Next", command=self.greet)
-        self.greet_button.pack()
+        
+#        self._master.grid_rowconfigure(1, weight=1)
+#        self._master.grid_columnconfigure(0, weight=1)
 
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
 
-    def greet(self):
+#        self._top_frame.grid(row=0, sticky="ew")
+
+
+        self._label = Label(self._master, text="Welcome to Artificial Intelligence BJ Simulator")
+        self._label.pack()
+        self._close_button = Button(self._master, text="Close", command=self._master.quit)
+        self._close_button.pack()
+        self._next_button = Button(self._master, text="Next", command=self.next)
+        self._next_button.pack()
+
+
+    def next(self):
         pass
+
 '''
 class SimConfig:
     def __init__(self, master):
@@ -51,6 +63,10 @@ class PlayerPlan:
     def greet(self):
         print("Greetings!")
 '''
-root = Tk()
-my_gui = Welcome(root)
-root.mainloop()
+def main():
+    root = Tk()
+    my_gui = Welcome(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
